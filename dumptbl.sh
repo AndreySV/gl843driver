cbca9c572e9b8415635a0ae705083344212e0532
Andreas Robinson <andr345@gmail.com>
2009-09-07 14:49:53 +0200

    dumptbl: change argument order

--- dumptbl.sh
+++ dumptbl.sh
@@ -4,13 +4,14 @@
 
 if [ $# != 3 ]; then
     echo "\n`basename "$0"` - "
-    echo "Print a 16-bit motor acceleration or gamma table as a list of decimal numbers."
+    echo "Print a 16-bit motor acceleration or gamma table as a series of decimal numbers."
 
-    echo "Usage: `basename $0` <start> <len> <file>"
-    echo "  start: start offset within the file (can be hex with a 0x prefix)"
-    echo "  len:   number of bytes to read"
+    echo "Usage: `basename $0` <file> <start> <len>"
     echo "  file:  binary usb dump"
+    echo "  start: start offset within the file (can be hex with a 0x prefix)"
+    echo "  len:   number of bytes to read (decimal only)"
     exit
 fi
 
-hexdump -s $1 -n $2 -e '1/2 "%d\n"' -v $3
+hexdump -s $2 -n $3 -e '1/2 "%u "' -v $1
+echo
